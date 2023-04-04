3c709bf9fa7b6c45898cb82e4380af72b888603e
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2023-04-05 00:14:16 +0300

    install graphql-yoga and setup base server and nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import { createSchema, createYoga } from 'graphql-yoga'
+import { createServer } from 'node:http'
 
-AppDataSource.initialize().then(async () => {
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello(name: String): String!
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: (_: any, { name } : any) => `Hello from ${name || "World"}`
+      }
+    }
+  })
+})
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+const server = createServer(yoga)
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
-
-}).catch(error => console.log(error))
+server.listen(4000, () => {
+  console.info('Server is running on http://localhost:4000/graphql')
+})
