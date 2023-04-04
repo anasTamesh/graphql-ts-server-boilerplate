1630d61c70697ad3ee79fc8e77707811bd1516e5
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2023-04-05 00:35:23 +0300

    setup user entity and install uuid for PrimaryColumn

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,20 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
-
+import { Entity, Column, PrimaryColumn, BeforeInsert, BaseEntity } from "typeorm"
+import { v4 as uuidv4 } from 'uuid';
 @Entity()
-export class User {
+export class User extends BaseEntity {
 
-    @PrimaryGeneratedColumn()
-    id: number
+    @PrimaryColumn("uuid")
+    id: string
 
-    @Column()
-    firstName: string
+    @Column("varchar", { length: 255 })
+    email: string
 
-    @Column()
-    lastName: string
+    @Column("text")
+    password: string
 
-    @Column()
-    age: number
+    @BeforeInsert()
+    addId() {
+        this.id = uuidv4();
+    }
 
 }
