c2bf6547c14bab326322366db072e4c53ff6140c
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2023-04-16 01:36:53 +0300

    cleaning index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { createSchema, createYoga } from 'graphql-yoga'
 import { createServer } from 'node:http'
 import { join } from 'node:path'
 import {resolvers} from './resolvers'
-// import { AppDataSource } from './data-source'
 import { createTypeormConn } from './utils/createTypeormConn'
 
 const schema = loadSchemaSync(join(__dirname, 'schema.graphql'), { loaders: [new GraphQLFileLoader()] })
@@ -22,9 +21,5 @@ export const startServer = async () => {
   await server.listen(4000);
   console.info('Server is running on http://localhost:4000/graphql');
 }
-// AppDataSource.initialize().then(()=>{
-//     server.listen(4000, () => {
-//       console.info('Server is running on http://localhost:4000/graphql')
-//     })
-// });
+
 startServer();
