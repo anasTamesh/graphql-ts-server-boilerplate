48cef6f845fb96fb639e04d2e91530e91fd6e96d
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2023-04-16 01:43:07 +0300

    cleaning register.test.ts

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,15 +1,13 @@
 import "reflect-metadata";
 import { request } from 'graphql-request';
 import { host } from './constants';
-// import { AppTestDataSource } from '../data-source';
 import { User } from '../entity/User';
 import { createTypeormConn } from "../utils/createTypeormConn";
 import { DataSource } from "typeorm";
 
 let dataSource : DataSource;
+
 beforeAll( async () => {
-  // dataSource = await AppTestDataSource.initialize();
-  // await AppTestDataSource.initialize();
   dataSource  = await createTypeormConn();
 })
 
@@ -25,12 +23,8 @@ mutation{
 test('Test register user', async () => {
     const response = await request(host, mutation);
     expect(response).toEqual({register: true});
-    
-    // const dataSource = await AppDataSource.initialize();
-    // const userRepository = dataSource.getRepository(User);
-    // const users = await userRepository.find( { where: { email }});
-    const users = await User.find( { where: { email }});
 
+    const users = await User.find( { where: { email }});
     expect(users).toHaveLength(1);
     const user = users[0];
     expect(user.email).toEqual(email);
