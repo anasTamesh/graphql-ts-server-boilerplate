5050bb021923942deaed23872e4e7bbe537e8c43
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2023-04-19 02:44:05 +0300

    clean up createTypeormConn.ts

--- src/utils/createTypeormConn.ts
+++ src/utils/createTypeormConn.ts
@@ -1,14 +1,5 @@
 import { AppDataSource } from "../data-source";
 
 export const createTypeormConn =  async () => {
-    // const connType = process.env.NODE_ENV;
-    // if (connType == "test"){
-    //     return await AppTestDataSource.initialize();
-    // } 
-    // else if (connType == "development"){
-    //     return await AppDataSource.initialize();
-    // } else {
-    //     return await AppDataSource.initialize();
-    // }
     return await AppDataSource.initialize();
 }
