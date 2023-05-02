a4b8d9f6a35f1c94f7cc03e4e721b970ff1adce2
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2023-05-03 01:42:12 +0300

    clean up startServer.ts

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,63 +1,28 @@
 import * as path from 'path'
 import { createYoga } from 'graphql-yoga'
-// import { createSchema, createYoga } from 'graphql-yoga'
 import { createServer } from 'node:http'
 import { createTypeormConn } from './utils/createTypeormConn'
 import { loadFilesSync } from '@graphql-tools/load-files'
 import { mergeResolvers } from '@graphql-tools/merge'
-// import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge'
 import {loadSchema} from '@graphql-tools/load'
 import {GraphQLFileLoader} from '@graphql-tools/graphql-file-loader'
 import { addResolversToSchema } from '@graphql-tools/schema'
-// import { GraphQLSchema } from 'graphql'
 
 export const startServer = async () => {
-  // let schemas = loadFilesSync(path.join(__dirname, './**/schema.graphql'));
-  // const shared = loadFilesSync(path.join(__dirname, './shared.graphql'));
-  // schemas.forEach(sc => {
-  //   shared.push(sc);
-  // });
-  // schemas = [shared[0], ...schemas]
-  // schemas.push(shared[0]);
-  // console.log('schemas');
-  // console.log(schemas);
-  const schema2 = await loadSchema(path.join(__dirname, './modules/**/schema.graphql'), {
-    // load files and merge them into a single schema object
+  const schemas = await loadSchema(path.join(__dirname, './modules/**/schema.graphql'), {
     loaders: [new GraphQLFileLoader()]
-  })
-  // console.log('schema2');
-  // console.log(schema2);
-  // const schema5 = await loadSchema('./src/**/*.graphql', { loaders: [new GraphQLFileLoader()] });
-  
-  // const typeDefs = mergeTypeDefs(schemas);
-  // console.log('typeDefs');
-  // console.log(typeDefs);
+  });
+
   const resolversArray = loadFilesSync(path.join(__dirname, './modules/**/resolvers.ts'));
   const resolvers = mergeResolvers(resolversArray);
-  // console.log('resolvers');
-  // console.log(resolvers);
-  
-  // const sc = createSchema({
-  //   typeDefs,
-  //   resolvers
-  // });
-  // console.log('sc');
-  // console.log(sc);
 
-  const sc2 = addResolversToSchema({
-    schema: schema2,
+  const schema = addResolversToSchema({
+    schema: schemas,
     resolvers
   });
-  // console.log('sc2');
-  // console.log(sc2);
-  // const yoga = createYoga({
-  //   schema: addResolversToSchema({
-  //     schema: schema2,
-  //     resolvers
-  //   })
-  // });
+
   const yoga = createYoga({
-    schema: sc2
+    schema
   });
   
   const server = createServer(yoga);
