156a5d8d05430696ad5f5b936cc696e6855b692a
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2023-06-07 02:35:14 +0300

    create setup.ts for global test

---
+++ src/testSetup/setup.ts
@@ -0,0 +1,18 @@
+import { startServer } from "../startServer";
+import { DataSource } from "typeorm";
+import { Server } from "net";
+import { AddressInfo } from "net";
+
+interface AppConn {
+    app: Server;
+    conn: DataSource;
+};
+  
+let appConn: AppConn;
+
+export const setup =  async () => {
+    appConn = await startServer();
+    const {port} = appConn.app.address() as AddressInfo;
+    process.env.TEST_HOST = `http://127.0.0.1:${port}/graphql`;
+    return appConn;
+};
