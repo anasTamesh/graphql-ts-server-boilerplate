a1038b0c6ff3526b0704c1d69ba194853014d950
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2023-06-07 03:54:25 +0300

    add express, redis and implement context in startServer

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,12 +1,15 @@
 import * as path from 'path'
 import { createYoga } from 'graphql-yoga'
-import { createServer } from 'node:http'
+// import { createServer } from 'node:http'
 import { createTypeormConn } from './utils/createTypeormConn'
 import { loadFilesSync } from '@graphql-tools/load-files'
 import { mergeResolvers } from '@graphql-tools/merge'
 import {loadSchema} from '@graphql-tools/load'
 import {GraphQLFileLoader} from '@graphql-tools/graphql-file-loader'
 import { addResolversToSchema } from '@graphql-tools/schema'
+import  Redis  from 'ioredis'
+import express = require('express')
+import { User } from './entity/User'
 
 export const startServer = async () => {
   const schemas = await loadSchema(path.join(__dirname, './modules/**/schema.graphql'), {
@@ -20,12 +23,34 @@ export const startServer = async () => {
     schema: schemas,
     resolvers
   });
+  
+  const redis = new Redis();
 
   const yoga = createYoga({
-    schema
+    schema,
+    context: request => {
+      const U: URL = new URL( request.request.url);
+      return {
+        redis,
+        url: U.protocol + "://" + U.host
+      }
+    }
   });
   
-  const server = createServer(yoga);
+  // const server = createServer(yoga);
+  const server = express();
+  server.use(yoga.graphqlEndpoint, yoga);
+  server.get('/confirm/:id', async (req, res) => {
+    const { id } = req.params;
+    const userId = await redis.get(id) as string;
+    if (userId) {
+      await User.update({ id: userId }, { confirmed: true });
+      res.send("ok");
+    } else {
+      res.send("invalid");
+    }
+  });
+  
   let port = process.env.NODE_ENV === 'test' ? 0 : 4000;
   await createTypeormConn(); 
   const app = server.listen({ port });
