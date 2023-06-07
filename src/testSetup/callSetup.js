9d6ec7590f71713748f6291bcf12ca7321c8e685
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2023-06-07 03:06:26 +0300

    set setup function to run if TEST_HOST is null

--- src/testSetup/callSetup.js
+++ src/testSetup/callSetup.js
@@ -6,6 +6,8 @@ const { setup } = require("./setup");
 
 module.exports = async function() {
   // Call your initialization methods here.
-  await setup();
+  if(!process.env.TEST_HOST){
+    await setup();
+  }
   return null;
 };
