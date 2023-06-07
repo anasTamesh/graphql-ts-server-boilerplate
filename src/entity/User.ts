9985133e0d70e6d90af435710cd9ae9caae92d15
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2023-06-07 03:47:53 +0300

    replace PrimaryColumn PrimaryGeneratedColumn and remove beforeAll function

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,9 +1,8 @@
-import { Entity, Column, PrimaryColumn, BeforeInsert, BaseEntity } from "typeorm"
-import { v4 as uuidv4 } from 'uuid';
+import { Entity, Column, BaseEntity, PrimaryGeneratedColumn } from "typeorm"
 @Entity("users")
 export class User extends BaseEntity {
 
-    @PrimaryColumn("uuid")
+    @PrimaryGeneratedColumn("uuid")
     id: string
 
     @Column("varchar", { length: 255 })
@@ -11,10 +10,5 @@ export class User extends BaseEntity {
 
     @Column("text")
     password: string
-
-    @BeforeInsert()
-    addId() {
-        this.id = uuidv4();
-    }
 
 }
