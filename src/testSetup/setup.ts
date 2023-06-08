f4cd2afd06033ae23e34f3ca1883af43d09d5b94
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2023-06-08 23:06:20 +0300

    add env for routes

--- src/testSetup/setup.ts
+++ src/testSetup/setup.ts
@@ -5,5 +5,6 @@ export const setup =  async () => {
     const appConn = await startServer();
     const {port} = appConn.address() as AddressInfo;
     process.env.TEST_HOST = `http://127.0.0.1:${port}/graphql`;
+    process.env.TEST_HOST2 = `http://127.0.0.1:${port}`;
     return appConn;
 };
