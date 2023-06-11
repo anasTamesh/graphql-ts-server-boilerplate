d124b4d41fdc521995bc8ba45b921cd358a68b75
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2023-06-11 20:39:52 +0300

    create confirmEmail test

--- src/utils/createConfirmEmailLink.test.ts
+++ src/utils/createConfirmEmailLink.test.ts
@@ -32,9 +32,4 @@ describe("Test createConfirmEmailLink", () => {
         expect(value).toBeNull();
     });
 
-    it("sends invalid back if a bad id was sent",async () => {
-       const response = await fetch(`${process.env.TEST_HOST}/confirm/1234`);
-       const text = await response.text();
-       expect(text).toEqual("invalid");
-    });
 });
